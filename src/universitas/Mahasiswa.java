cca830251fb3991816d03dc7d39e91fe1f0a9905
fahmi341 <fahmirezkimulyawan3@gmail.com>
2023-08-04 01:11:09 +0700

    Melengkapi class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -4,8 +4,39 @@ public class Mahasiswa {
     private String npm;
     private String nama;
     private String prodi;
+
+    public Mahasiswa(String npm, String nama, String prodi) {
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
     
 }
+
 
 public String getDetail(){
 
